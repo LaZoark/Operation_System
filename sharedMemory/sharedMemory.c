0b16c3bd9b4d81822c8e5916a083696043903f81
LaZoark <emersonchen086@icloud.com>
2021-03-07 02:24:11 +0800

    Sweet! second mission completed.(thanks to pipe)

--- sharedMemory/sharedMemory.c
+++ sharedMemory/sharedMemory.c
@@ -7,12 +7,23 @@
 
 int main()
 {
+    int fd[2];
+    // create pipe descriptors
+    pipe(fd);
+
     /* the identifier for the shared memory segment */
     int segment_id;
     /* a pointer to the shared memory segment */
     char *shared_memory;
     /* the size (in bytes) of the shared memory segment */
     const int segment_size = 4096;
+    /** allocate  a shared memory segment */
+    segment_id = shmget(IPC_PRIVATE, segment_size, S_IRUSR | S_IWUSR); // assigning the "id" of segment to "segment_id"
+    /** attach the shared memory segment */
+    shared_memory = (char *)shmat(segment_id, NULL, 0);
+    printf("shared memory segment %d attached at address %p\n", segment_id, shared_memory);
+    /** write a message to the shared memory segment   */
+    sprintf(shared_memory, "Hi there!");
 
     pid_t PID = fork(); // 從呼叫 fork 開始, 會分成兩支程序多工進行
     switch (PID)
@@ -21,25 +32,27 @@ int main()
         perror("fork()");
         exit(-1);
     case 0: // PID == 0 代表是子程序
+        // child: reading only, so close the write-descriptor
+		close(fd[1]);
+        // now read the data (will block until it succeeds)
+		read(fd[0], &shared_memory, sizeof(shared_memory));
         printf("[Child] Child's PID is %d\n", getpid());
-        sleep(5);
-        printf("shared memory segment %d attached at address %p\n", segment_id, shared_memory);
-        printf("shared content : *%s*\n", shared_memory);
+        printf("passed content : *%s*\n", shared_memory);
+        // close the read-descriptor
+		close(fd[0]);
         break;
     default: // PID > 0 代表是父程序
+        // parent: writing only, so close read-descriptor.
+        close(fd[0]);
+        // send the content on the write-descriptor.
+        write(fd[1],&shared_memory,sizeof(shared_memory));
         printf("[Parent] Parent's PID is %d\n", getpid());
-
-        /** allocate  a shared memory segment */
-        segment_id = shmget(IPC_PRIVATE, segment_size, S_IRUSR | S_IWUSR); // assigning the "id" of segment to "segment_id"
-        /** attach the shared memory segment */
-        shared_memory = (char *)shmat(segment_id, NULL, 0);
-        printf("shared memory segment %d attached at address %p\n", segment_id, shared_memory);
-        /** write a message to the shared memory segment   */
-        sprintf(shared_memory, "Hi there!");
         /** now print out the string from shared memory */
         printf("shared content : *%s*\n", shared_memory);
 
         wait(NULL);
+        // close the write descriptor
+		close(fd[1]);
     }
 
     /** now detach the shared memory segment */
