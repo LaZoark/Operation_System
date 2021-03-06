9c78c5f6bd6870f082307083e778ed4ed63696ad
LaZoark <emersonchen086@icloud.com>
2021-03-07 00:34:49 +0800

    preparing for modification

--- sharedMemory/sharedMemory.c
+++ sharedMemory/sharedMemory.c
@@ -7,16 +7,20 @@
 
 int main()
 {
-    pid_t id_child = fork();
-    if (id_child == -1) // PID == -1 代表 fork 出錯
+    pid_t PID = fork();// 從呼叫 fork 開始, 會分成兩支程序多工進行
+    switch(PID)
     {
-        perror("fork()");
-        exit(-1);
+        case -1: // PID == -1 代表 fork 出錯
+            perror("fork()");
+            exit(-1);
+        case 0: // PID == 0 代表是子程序
+            printf("[Child] Child's PID is %d\n",getpid());
+            
+            break;
+        default: // PID > 0 代表是父程序
+            printf("[Parent] Parent's PID is %d\n",getpid());
+            wait(NULL);
     }
-    else if (id_child == 0) // PID == 0 代表是子程序
-        printf("[Child] Child's PID is   %d\n", getpid());
-    else // PID > 0 代表是父程序
-        printf("[Parent] Parent's PID is %d\n", getpid());
 
     /* the identifier for the shared memory segment */
     int segment_id;
