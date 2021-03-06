5c104fbf6805211186367ac591cddc7412026f29
LaZoark <emersonchen086@icloud.com>
2021-03-07 01:49:34 +0800

    v2, it works!

--- sharedMemory/passToChild.c
+++ sharedMemory/passToChild.c
@@ -1,55 +1,35 @@
-// C program for passing value from 
-// child process to parent process 
-#include <pthread.h> 
-#include <stdio.h> 
-#include <sys/types.h> 
-#include <unistd.h>  
-#include <stdlib.h>  
-#include <sys/wait.h> 
-#define MAX 10 
-  
-int main() 
-{ 
-  
-  int fd[2], i = 0; 
-  pipe(fd); 
-  pid_t pid = fork(); 
-  
-   if(pid > 0) { 
-      wait(NULL); 
-  
-      // closing the standard input  
-      close(0); 
-  
-      // no need to use the write end of pipe here so close it 
-      close(fd[1]);  
-  
-       // duplicating fd[0] with standard input 0 
-      dup(fd[0]);  
-      int arr[MAX]; 
-  
-      // n stores the total bytes read successfully 
-      int n = read(fd[0], arr, sizeof(arr)); 
-      for ( i = 0;i < n/4; i++) 
-  
-         // printing the array received from child process 
-          printf("%d ", arr[i]);  
-  }  
-  else if( pid == 0 ) { 
-      int arr[] = {1, 2, 3, 4, 5}; 
-  
-      // no need to use the read end of pipe here so close it 
-      close(fd[0]);  
-  
-       // closing the standard output 
-      close(1);     
-  
-      // duplicating fd[0] with standard output 1 
-      dup(fd[1]);   
-      write(1, arr, sizeof(arr)); 
-  }  
-  
-  else { 
-      perror("error\n"); //fork() 
-  } 
-}  
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(int argc, char *argv[]) {
+	int fd[2];
+	int childID = 0;
+
+	// create pipe descriptors
+	pipe(fd);
+
+	// fork() returns 0 for child process, child-pid for parent process.
+	if (fork() != 0) {
+		// parent: writing only, so close read-descriptor.
+		close(fd[0]);
+
+		// send the childID on the write-descriptor.
+		childID = 1;
+		write(fd[1], &childID, sizeof(childID));
+		printf("Parent(%d) send childID: %d\n", getpid(), childID);
+
+		// close the write descriptor
+		close(fd[1]);
+	} else {
+		// child: reading only, so close the write-descriptor
+		close(fd[1]);
+
+		// now read the data (will block until it succeeds)
+		read(fd[0], &childID, sizeof(childID));
+		printf("Child(%d) received childID: %d\n", getpid(), childID);
+
+		// close the read-descriptor
+		close(fd[0]);
+	}
+	return 0;
+}
