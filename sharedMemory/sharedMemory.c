d8c73841cc39ca32a457bbe397a74e1602ba4440
LaZoark <emersonchen086@icloud.com>
2021-03-10 20:52:27 +0800

    it turns out that no need to use pipe

--- sharedMemory/sharedMemory.c
+++ sharedMemory/sharedMemory.c
@@ -7,17 +7,14 @@
 
 int main()
 {
-    int fd[2];
-    // create pipe descriptors
-    pipe(fd);
-    /* the identifier for the shared memory segment */
-    int segment_id;
-    /* a pointer to the shared memory segment */
-    char *shared_memory;
     /* the size (in bytes) of the shared memory segment */
     const int segment_size = 4096;
+    /* the identifier for the shared memory segment */
+    int segment_id;
     /** allocate a shared memory segment */
     segment_id = shmget(IPC_PRIVATE, segment_size, S_IRUSR | S_IWUSR);
+    /* a pointer to the shared memory segment */
+    char *shared_memory;
 
     pid_t PID = fork(); // 從呼叫 fork 開始, 會分成兩支程序多工進行
     switch (PID)
@@ -26,51 +23,35 @@ int main()
         perror("fork()");
         exit(-1);
     case 0: // PID == 0 代表是子程序
-        // child: reading only, so close the write-descriptor
-        close(fd[1]);
 
         /** attach the shared memory segment */
         shared_memory = (char *)shmat(segment_id, NULL, 0);
         printf("[Child]shared memory segment %d attached at address %p\n", segment_id, shared_memory);
-
-        // now read the data (will block until it succeeds)
-        read(fd[0], &shared_memory, sizeof(shared_memory));
         printf("[Child] Child's PID is %d\n passed content : *%s*\n", getpid(), shared_memory);
 
-        // close the read-descriptor
-        close(fd[0]);
         break;
     default: // PID > 0 代表是父程序
-        // parent: writing only, so close read-descriptor.
-        close(fd[0]);
-        // sleep(1);
         /** attach the shared memory segment */
         shared_memory = (char *)shmat(segment_id, NULL, 0);
         printf("[Parent]shared memory segment %d attached at address %p\n", segment_id, shared_memory);
         /** write a message to the shared memory segment   */
-        sprintf(shared_memory, PID==0?"WHAT?":"Hi there!");
-
-        // send the content on the write-descriptor.
-        write(fd[1], &shared_memory, sizeof(shared_memory));
+        sprintf(shared_memory, PID == 0 ? "WHAT?" : "Hi there!");
         /** now print out the string from shared memory */
         printf("[Parent] Parent's PID is %d\n shared content : *%s*\n", getpid(), shared_memory);
 
         wait(NULL);
-        // close the write descriptor
-        close(fd[1]);
+
+        /** now detach the shared memory segment */
+        if (shmdt(shared_memory) == -1)
+            fprintf(stderr, "Unable to detach\n");
+        else
+        {
+            /** now remove the shared memory segment */
+            shmctl(segment_id, IPC_RMID, NULL);
+            printf("detach was done.\n");
+        }
     }
 
-    /** now detach the shared memory segment */
-    if (shmdt(shared_memory) == -1)
-    {
-        fprintf(stderr, "Unable to detach\n");
-    }
-    else
-    {
-        /** now remove the shared memory segment */
-        shmctl(segment_id, IPC_RMID, NULL);
-        printf("detach was done.\n");
-    }
     return 0;
 }
 // 把4096bytes指定給"某記憶體位置"(也就是segment_id)
