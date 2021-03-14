10df941936b23e933e16234adeeaa02616646d79
LaZoark <emersonchen086@icloud.com>
2021-03-14 22:21:59 +0800

    dead loop

--- thread/thread.c
+++ thread/thread.c
@@ -4,15 +4,23 @@
 #define SOURCE "Lenna.bmp"
 #define DEST "new_Lenna.bmp"
 
-static int data_size=0;
-// int count = 0;
+static int data_size = 0;
+int count = 0;
 
-void *child(void* data_)
+void *child(void *data_)
 {
-    char* d = (char* )data_;
+    char *d = (char *)data_;
     FILE *newfp = fopen(DEST, "wb+");
-    for (int i = 0; i < data_size; i++)
-        fwrite(&d[i], sizeof(char), 1, newfp);
+    int i=0;
+    while(i<data_size)
+    {
+        if(i<count)
+            fwrite(&d[i++], sizeof(char), 1, newfp);
+    }
+    // for (int i = 0; i < data_size; i++)
+    //     if (i < count)
+    //         fwrite(&d[i], sizeof(char), 1, newfp);
+    //     else i--;
     fclose(newfp) ? perror("fclose()") : printf("Child was done.\n");
     pthread_exit(NULL); // no need to return
 }
@@ -21,16 +29,16 @@ int main()
 {
     FILE *file = fopen(SOURCE, "rb");
     fseek(file, 0, SEEK_END) ? perror("fseek()") : 1; // go through the file
-    data_size = ftell(file);                      // tell how far it goes
+    data_size = ftell(file);                          // tell how far it goes
     fclose(file);
     printf("Image size:%d(Bytes)\n", data_size);
 
-    char *data = calloc(data_size,sizeof(char)); // memory allocation
+    char *data = calloc(data_size, sizeof(char)); // memory allocation
 
     pthread_t t;
 
     // 建立子執行緒
-    pthread_create(&t, NULL, child, (void* )data);
+    pthread_create(&t, NULL, child, (void *)data);
 
     FILE *fp = fopen(SOURCE, "rb");
     if (fp != NULL)
@@ -38,18 +46,19 @@ int main()
     else
         perror("fopen(SOURCE)");
     char buffer;
-    printf("sizeof(buffer)");
+    // printf("sizeof(buffer)=%ld\n",sizeof(buffer));
     for (int j = 0; j < data_size; j++)
     {
         fread(&buffer, sizeof(char), 1, fp);
         *(data + j) = buffer;
-        // printf("data+j= %p\n",*(data+j));
+        count = j;
     }
     fclose(fp);
-    
-    sleep(1); // wait for the data(child) ready 
-               
+    // printf("sizeof(buffer)=%ld\n",sizeof(buffer));
+
+    sleep(1); // wait for the data(child) ready
+
     pthread_join(t, NULL); // 等待子執行緒執行完成
-    free(data);  // 釋放記憶體
+    free(data);            // 釋放記憶體
     return 0;
 }
