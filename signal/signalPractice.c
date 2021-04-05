def052ecd4d7840116f596fb725bd254c5c4cfcf
LaZoark <emersonchen086@icloud.com>
2021-04-05 22:27:57 +0800

    without handler

--- signal/signalPractice.c
+++ signal/signalPractice.c
@@ -5,12 +5,12 @@ void SIGFPE_Handler(int);
 int main()
 {
     int a, b;
-    if (signal(SIGFPE, SIGFPE_Handler) == SIG_ERR)
-    {
-        perror("Set up signal handler error!");
-        abort();
-    }
-    printf("If you divided some integer by 0,\nit can start the signal handler..\n");
+    // if (signal(SIGFPE, SIGFPE_Handler) == SIG_ERR)
+    // {
+    //     perror("Set up signal handler error!");
+    //     abort();
+    // }
+    // printf("If you divided some integer by 0,\nit can start the signal handler..\n");
     printf("Now we can input two integers for division.\n");
     scanf("%d %d", &a, &b);
     printf("%d / %d = %d\n", a, b, a / b);
